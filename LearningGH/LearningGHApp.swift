fe11ff527fe8d04a5fcdafaa1ecac6f3b45a9860
teresa <ying.schaeffer70@gmail.com>
2021-04-28 10:07:14 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by teresa schaeffer on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
